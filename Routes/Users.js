b651c72d451684732ba13c1f181b3234e9b4d5a1
fazliddin <fazliddinnarkhajayev@gmail.com>
2023-11-01 17:52:36 +0500

    added-tmp-str

--- Routes/Users.js
+++ Routes/Users.js
@@ -2035,7 +2035,7 @@ users.post('/uploadImage', upload.single('file'), async (req, res) => {
     let connect,
         userInfo = await jwt.decode(req.headers.authorization.split(' ')[1]),
         appData = {status: false},
-        dir ='',
+        dir ='/tmp/',
         dirPreview ='',
         typeUser = req.body.typeUser,
         typeImage = req.body.typeImage;
