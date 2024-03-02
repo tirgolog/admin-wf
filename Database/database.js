3f6c74794a67f3604d074b749ef638cd815355fd
nemofalcon <nemo.falcon96@gmail.com>
2024-03-02 10:28:25 +0500

    refernce

--- Database/database.js
+++ Database/database.js
@@ -1,7 +1,7 @@
 const
     mysql = require('mysql2/promise'),
     connection = mysql.createPool({
-        host:'13.233.208.188',
+        host:'localhost',
         user:'root',
         password:'tirgO@01',
         database:'tirgo',
