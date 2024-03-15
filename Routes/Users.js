13ba04fef4dff7575189ac4a0543693b2b7f628a
nemofalcon <nemo.falcon96@gmail.com>
2024-03-15 16:26:09 +0500

    users

--- Routes/Users.js
+++ Routes/Users.js
@@ -3731,6 +3731,7 @@ users.get("/getMyOrdersDriver", async (req, res) => {
       "SELECT * FROM users_transport WHERE user_id = ? AND active = 1",
       [userInfo.id]
     );
+    console.log(transports, 'transports');
     for (let transport of transports) {
       transportstypes = transportstypes + transport.type + ",";
     }
@@ -3740,6 +3741,7 @@ users.get("/getMyOrdersDriver", async (req, res) => {
       "SELECT o.*,ul.name as usernameorder,ul.phone as userphoneorder FROM orders o LEFT JOIN users_list ul ON o.user_id = ul.id WHERE o.status <> 2 ORDER BY o.id DESC",
       [transportstypes, transportstypes]
     );
+    console.log(rows,'rows');
     if (rows.length) {
       appData.data = await Promise.all(
         [...merchantData, ...rows].map(async (item) => {
