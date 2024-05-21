c1deb07715f20957dcf8910077ddc3c8d12d8d9f
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-05-22 02:54:03 +0500

    add

--- Modules/Socket.js
+++ Modules/Socket.js
@@ -20,7 +20,7 @@ module.exports = {
                 currentDate = new Date();
             try {
                 connectOnTop = await database.connection.getConnection();
-                await connectOnTop.query('UPDATE users_list SET status = 1,date_last_login = ? WHERE id = ?', [currentDate,socket.userid]);
+                await connectOnTop.query('UPDATE users_list SET status = 1,date_last_login = ? WHERE id = ?', [currentDate, socket.userid]);
             } catch (e) {
                 console.log(e)
             } finally {
@@ -28,16 +28,48 @@ module.exports = {
                     connectOnTop.release()
                 }
             }
-            socket.join('s'+socket.userid);
-            console.log('Пользователь '+socket.userid+' подключился к системной комнате #' + 's'+socket.userid);
-            socket.sroomid = 's'+socket.userid;
-            socket.on('disconnect',async function(){
-                console.log('Пользователь '+socket.userid+' отключился');
-                socketIO.emit('users-changed', {event: 'disconnected',userid:socket.userid});
+            socket.join('s' + socket.userid);
+            console.log('Пользователь ' + socket.userid + ' подключился к системной комнате #' + 's' + socket.userid);
+            socket.sroomid = 's' + socket.userid;
+            socket.on('disconnect', async function () {
+                console.log('Пользователь ' + socket.userid + ' отключился');
+                socketIO.emit('users-changed', { event: 'disconnected', userid: socket.userid });
                 let connect;
                 try {
                     connect = await database.connection.getConnection();
                     await connect.query('UPDATE users_list SET status = 0 WHERE id = ?', [socket.userid]);
+                } catch (e) {
+                    console.log(e)
+                } finally {
+                    if (connect) {
+                        connect.release()
+                    }
+                }
+            });
+
+            socket.on('update-service-users-list', async function (data) {
+                let connect;
+                try {
+                    console.log('update-service-users-list', data)
+                    socketIO.emit('update-service-users-list', 'data')
+                    // connect = await database.connection.getConnection();
+                    // await connect.query('UPDATE users_list SET status = 0 WHERE id = ?', [socket.userid]);
+                } catch (e) {
+                    console.log(e)
+                } finally {
+                    if (connect) {
+                        connect.release()
+                    }
+                }
+            });
+
+            socket.on('update-service-messages', async function (data) {
+                let connect;
+                try {
+                    console.log('update-service-messages')
+                    socketIO.emit('update-service-messages', data)
+                    // connect = await database.connection.getConnection();
+                    // await connect.query('UPDATE users_list SET status = 0 WHERE id = ?', [socket.userid]);
                 } catch (e) {
                     console.log(e)
                 } finally {
@@ -74,7 +106,7 @@ function getClients(roomid) {
 
     return new Promise((resolve, reject) => {
         socketIO.of('/').in(roomid).clients((error, clients) => {
-            if(error)
+            if (error)
                 return reject(error);
 
             resolve(clients);
