6211548f9b16e744bdaa8e78ada1cba2a222a039
fazliddinnarkhajayev <fazliddinnarkhajayev@gmail.com>
2024-09-21 20:13:00 +0500

    text-on-request-service

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -4390,6 +4390,23 @@ admin.post("/agent/add-services", async (req, res) => {
       `, [insertValues]);
         if (result.affectedRows > 0) {
           appData.status = true;
+
+        for(let service of services) {
+          const insertData = await connect.query(`
+          INSERT INTO service_bot_message set 
+            message_type = ?,
+            message = ?,
+            message_sender_type = ?,
+            sender_user_id = ?
+          `, [
+              'text',
+              `Пользователь отправил запрос на ${service[0]?.code} ${service[0]?.name}`,
+              userInfo.user_type == 5 ? 'tms-user' : 'agent',
+              user_id
+            ]
+             );
+        }
+
           socket.updateAllMessages("update-alpha-balance", "1");
           res.status(200).json(appData);
         }
