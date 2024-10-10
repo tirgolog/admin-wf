b072c1e9f1220b5e6db7a62a70a4b7523d27d377
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-10-10 19:31:40 +0500

    add

--- Routes/Users.js
+++ Routes/Users.js
@@ -737,7 +737,7 @@ users.post("/login", async (req, res) => {
           [code, phone]
         );
         await connect.query(
-          "INSERT INTO users_contacts SET is_tg = ?, verify_code=?,text=?,user_type = 1,user_id = ?, verify_code_date_time",
+          "INSERT INTO users_contacts SET is_tg = ?, verify_code=?,text=?,user_type = 1,user_id = ?, verify_code_date_time = ?",
           [isTelegram, code, phone, insert.insertId, new Date().getTime()]
         );
         appData.status = true;
