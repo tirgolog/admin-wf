fbb73d6995a269c31efeaa37798fdbb0461ceafa
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-10-11 15:20:45 +0500

    add

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -4954,7 +4954,7 @@ admin.post("/services-transaction/status/by", async (req, res) => {
     connect = await database.connection.getConnection();
     let user;
     [user] = await connect.query(
-      `SELECT sbu.chat_id, s.id serviceId, s.name serviceName, ul.id user_id, st.amount_tir serviceAmount, st.is_by_agent, st.agent_id, ul.driver_group_id groupId, dg.chat_id groupChatId, dg.owner_phone_number groupOwnerPhoneNumber FROM tir_balance_transaction st
+      `SELECT st.id id, sbu.chat_id, s.id serviceId, s.name serviceName, ul.id user_id, st.amount_tir serviceAmount, st.is_by_agent, st.agent_id, ul.driver_group_id groupId, dg.chat_id groupChatId, dg.owner_phone_number groupOwnerPhoneNumber FROM tir_balance_transaction st
       LEFT JOIN services_bot_users sbu on sbu.user_id = st.user_id
       LEFT JOIN users_list ul on ul.id = st.user_id
       LEFT JOIN driver_group dg on dg.id = ul.driver_group_id
@@ -5023,7 +5023,7 @@ admin.post("/services-transaction/status/by", async (req, res) => {
           console.log(user)
           const text = `Заказ #${user[0]?.serviceId} выполнен. Все детали успешно завершены`;
           if(user[0]?.groupChatId) {
-            socket.emit(14, 'service-status-change', JSON.stringify({ userChatId: user[0]?.groupChatId, text }));
+            socket.emit(14, 'service-status-change', JSON.stringify({ userChatId: user[0]?.id, text }));
           }
           await sendTextSms(user[0]?.groupOwnerPhoneNumber, text)
         }
