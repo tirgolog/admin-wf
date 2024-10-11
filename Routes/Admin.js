7a26cd13489d982da4c69a97dccb55c8d06aec75
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-10-11 15:31:27 +0500

    add

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -5021,7 +5021,7 @@ admin.post("/services-transaction/status/by", async (req, res) => {
         socket.emit(14, 'service-status-change', JSON.stringify({ userChatId: user[0]?.chat_id, text: `Услуга "${user[0]?.serviceName}" выполнен` }));
         if(user[0]?.groupId) {
           console.log(user)
-          const text = `Заказ #${user[0]?.serviceId} выполнен. Все детали успешно завершены`;
+          const text = `Запрос на услугу #${user[0]?.serviceId} выполнен. Все детали успешно завершены`;
           if(user[0]?.groupChatId) {
             socket.emit(14, 'service-status-change', JSON.stringify({ userChatId: user[0]?.id, text }));
           }
