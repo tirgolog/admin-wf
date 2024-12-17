1ecfda0696d708ea253ae5ca71a5578995cb4afc
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-12-17 23:39:28 +0500

    add

--- Database/database.js
+++ Database/database.js
@@ -1,12 +1,12 @@
-const  mysql = require('mysql2/promise'),
+const mysql = require('mysql2/promise'),
     connection = mysql.createPool({
-        host:'13.126.49.1',
-        user:'root',
-        password:'tirgO@01',
-        database:'tirgo',
+        host: '13.234.122.56',
+        user: 'root',
+        password: 'tirgO@01',
+        database: 'tirgo',
         port: 3306,
         debug: false,
         multipleStatements: true,
-});
+    });
 
 module.exports.connection = connection;
