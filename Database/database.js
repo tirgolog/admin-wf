f44f2b090bbeaffc3ad85520bd4d38d329e3ae72
fazliddin <fazliddinnarkhajayev@gmail.com>
2025-01-09 10:27:01 +0500

    add

--- Database/database.js
+++ Database/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise'),
     connection = mysql.createPool({
-        host: '13.126.95.31',
+        host: '13.233.97.167',
         user: 'root',
         password: 'tirgO@01',
         database: 'tirgo',
